25e4efcec462022f258acd9ee04a53479e6c8737
markakisdong <mark840205@gmail.com>
2017-01-14 01:26:53 +0800

    a long way to go...

--- main.py
+++ main.py
@@ -18,11 +18,23 @@ from configs import config_train
 from model import SoftAttentionModel 
 
 def train(params, data_loader):
-
     model = SoftAttentionModel(params, data_loader.n_words, data_loader.maxlen, data_loader.bivector)
     loss, context, sentence, mask = model.build()
+    train_op = tf.train.RMSPropOptimizer(params.learning_rate)
+    merged = tf.summary.merge_all()
+
+    init_op = tf.global_variables_initializer()
+    saver = tf.trian.Saver()
+    # Go go lamigo
+    sess = tf.Session()
+    sess.run(init_op)
+    train_writer = tf.train.SummaryWriter(params.log_dir, sess.graph)
+    if params.pretrained_path is not None:
+        print ('Starting with pretrained model: {}'.format(params.pretrained_path))
+        saver.restore(sess, params.pretrained_path)
 
 if __name__ == '__main__':
     params = config_train()
     data_loader = Data_loader(params)
-    train(params, data_loader)
+    pdb.set_trace()
+    #train(params, data_loader)
